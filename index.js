b5c2104de28943348bfa9a4e4077b27bcddd10ae
vincent-alamelle <vincent.alamelle@etu.univ-amu.fr>
2020-11-12 18:53:32 +0100

    First

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-//const math = require('mathjs');
+const math = require('mathjs');
 const bot = new Discord.Client;
 const db = require('./db')
 
@@ -662,80 +662,80 @@ bot.on("message", function (message) {
 //----------------------------------------------------------------------------------------------------//
 
 
-//     //-----------------------------------------Random--------------------------------------------------//
-//     function getRandomInt(max) {
-//         return Math.floor(Math.random() * Math.floor(max) + 1);
-//     }
-//     //-------------------------------------------------------------------------------------------//
-//
-//
-//     //-----------------------------------------Dice--------------------------------------------------//
-//     if (message.content.startsWith("!r")){
-//         let args = message.content.split(' ');
-//         args.shift();
-//         let str = args[0];
-//         let cpt = 0;
-//         let answer = "";
-//         let finalAnswer = "";
-//         let value = 0;
-//
-//         const calc = str.split("+")
-//         let chars;
-//
-//
-//         //let nb_dice = chars[0];
-//         //let size_dice = chars[1];
-//         let nb_dice;
-//         let size_dice;
-//
-//         for (let i = 0; i < calc.length; i++) {
-//             chars = calc[i].split('d');
-//             if (chars.length < 2){
-//                 message.reply("Enculé tapes la bonne commande !");
-//                 return;
-//             }
-//             nb_dice = chars[0]
-//             size_dice = chars[1];
-//             if(nb_dice === "") nb_dice = 1;
-//             if (size_dice === "")  size_dice = 1;
-//             console.log(nb_dice + " " + size_dice);
-//
-//             answer += "(";
-//             for (let i = 1; i <= nb_dice; i++) {
-//                 value = getRandomInt(size_dice);
-//                 answer += value + " + ";
-//                 cpt += value;
-//             }
-//             answer = answer.substring(0, answer.length - 3);
-//             answer += ") + ";
-//         }
-//
-//         if (calc.length > 1 || nb_dice > 1){
-//             finalAnswer = answer.substring(0, answer.length - 3);
-//             finalAnswer += " = " + cpt;
-//         }
-//         else finalAnswer = answer.substring(1, answer.length - 4);
-//
-//         if (finalAnswer.length > 1024 && finalAnswer.length < 1940){
-//             message.channel.send("Ca faisait beaucoup donc je te le mets en dur\nRésultat = " + finalAnswer);
-//         }
-//         else if (finalAnswer.length > 2000) {
-//             message.channel.send("Ca faisait beaucoup donc je te le mets en dur\nRésultat = " + cpt);
-//         }
-//         else {
-//             const embed = new Discord.MessageEmbed()
-//                 .setColor(0xfffff)
-//                 .setTitle('Lancer de dé')
-//                 .addField('Lancé', `\`\`\`js\n${args[0]}\`\`\``)
-//                 .addField('Résultat', `\`\`\`js\n${finalAnswer}\`\`\``)
-//
-//             message.channel.send(embed);
-//         }
-//
-//
-//
-//     }
-// //-------------------------------------------------------------------------------------------------//
+    //-----------------------------------------Random--------------------------------------------------//
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max) + 1);
+    }
+    //-------------------------------------------------------------------------------------------//
+
+
+    //-----------------------------------------Dice--------------------------------------------------//
+    if (message.content.startsWith("!r")){
+        let args = message.content.split(' ');
+        args.shift();
+        let str = args[0];
+        let cpt = 0;
+        let answer = "";
+        let finalAnswer = "";
+        let value = 0;
+
+        const calc = str.split("+")
+        let chars;
+
+
+        //let nb_dice = chars[0];
+        //let size_dice = chars[1];
+        let nb_dice;
+        let size_dice;
+
+        for (let i = 0; i < calc.length; i++) {
+            chars = calc[i].split('d');
+            if (chars.length < 2){
+                message.reply("Enculé tapes la bonne commande !");
+                return;
+            }
+            nb_dice = chars[0]
+            size_dice = chars[1];
+            if(nb_dice === "") nb_dice = 1;
+            if (size_dice === "")  size_dice = 1;
+            console.log(nb_dice + " " + size_dice);
+
+            answer += "(";
+            for (let i = 1; i <= nb_dice; i++) {
+                value = getRandomInt(size_dice);
+                answer += value + " + ";
+                cpt += value;
+            }
+            answer = answer.substring(0, answer.length - 3);
+            answer += ") + ";
+        }
+
+        if (calc.length > 1 || nb_dice > 1){
+            finalAnswer = answer.substring(0, answer.length - 3);
+            finalAnswer += " = " + cpt;
+        }
+        else finalAnswer = answer.substring(1, answer.length - 4);
+
+        if (finalAnswer.length > 1024 && finalAnswer.length < 1940){
+            message.channel.send("Ca faisait beaucoup donc je te le mets en dur\nRésultat = " + finalAnswer);
+        }
+        else if (finalAnswer.length > 2000) {
+            message.channel.send("Ca faisait beaucoup donc je te le mets en dur\nRésultat = " + cpt);
+        }
+        else {
+            const embed = new Discord.MessageEmbed()
+                .setColor(0xfffff)
+                .setTitle('Lancer de dé')
+                .addField('Lancé', `\`\`\`js\n${args[0]}\`\`\``)
+                .addField('Résultat', `\`\`\`js\n${finalAnswer}\`\`\``)
+
+            message.channel.send(embed);
+        }
+
+
+
+    }
+//-------------------------------------------------------------------------------------------------//
 
 
 
